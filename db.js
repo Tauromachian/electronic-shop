1b56303dab3f87052e881aec729380978998f57d
Jose Garcia <jogarcia@tuta.io>
2022-06-06 17:51:49 -0400

    Check connection

--- db.js
+++ db.js
@@ -5,4 +5,9 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_URI);
 
+sequelize
+  .authenticate()
+  .then(() => debug("Connection has been established successfully"))
+  .catch((error) => debug("Unable to connect to the database:" + error));
+
 module.exports = sequelize;
