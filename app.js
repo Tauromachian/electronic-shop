8d2a48e8aec8acac3b7e052abaa0c4e178a99c15
Jose Garcia <jogarcia@tuta.io>
2022-06-10 20:54:38 -0400

    Redirect to swagger

--- app.js
+++ app.js
@@ -22,4 +22,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/docs");
+});
+
 module.exports = app;
